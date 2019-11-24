2e123e150e6a67c0e55750fa01a02f9d8d689b0b
Nitin Kumar <snitin315@gmail.com>
2019-11-24 14:30:05 +0530

    add react-reveal

--- pages-sections/LandingPage-Sections/ProductSection.js
+++ pages-sections/LandingPage-Sections/ProductSection.js
@@ -2,14 +2,12 @@ import React from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 
-// @material-ui/icons
-import Chat from "@material-ui/icons/Chat";
-import VerifiedUser from "@material-ui/icons/VerifiedUser";
-import Fingerprint from "@material-ui/icons/Fingerprint";
+
 // core components
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import InfoArea from "components/InfoArea/InfoArea.js";
+import Fade from 'react-reveal/Fade';
 
 import styles from "assets/jss/nextjs-material-kit/pages/landingPageSections/productStyle.js";
 
@@ -24,6 +22,7 @@ const flex = {
 
 export default function ProductSection() {
   const classes = useStyles();
+  
   return (
     <div className={classes.section}>
       <GridContainer justify="center">
@@ -52,15 +51,11 @@ export default function ProductSection() {
       </GridContainer>
       <div>
         <GridContainer>
-          <GridItem xs={12} sm={12} md={4}>
-            
-          </GridItem>
-          <GridItem xs={12} sm={12} md={4}>
-            
-          </GridItem>
-          <GridItem xs={12} sm={12} md={4}>
-          
-          </GridItem>
+            <GridItem xs={12} sm={12} md={6} >
+              <Fade up delay={500}>
+               
+              </Fade>
+            </GridItem>
         </GridContainer>
       </div>
     </div>
