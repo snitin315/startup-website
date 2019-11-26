5fe496aff382def3fa580590412a276f4d9156b3
Nitin Kumar <snitin315@gmail.com>
2019-11-26 10:44:41 +0530

    update particle.js and styles

--- components/Particle/Particle.js
+++ components/Particle/Particle.js
@@ -19,107 +19,21 @@ const ParticlesContainer = (props) => {
             params={{
                 "particles": {
                     "number": {
-                        "value": 500,
-                        "density": {
-                            "enable": true,
-                            "value_area": 1803.4120608655228
-                        }
-                    },
-                    "color": {
-                        "value": "#fff"
-                    },
-                    "shape": {
-                        "type": "circle",
-                        "stroke": {
-                            "width": 5,
-                            "color": "#fff"
-                        },
-                        "polygon": {
-                            "nb_sides": 4
-                        },
-                    },
-                    "opacity": {
-                        "value": 0.4008530152163807,
-                        "random": false,
-                        "anim": {
-                            "enable": false,
-                            "speed": 2,
-                            "opacity_min": 0.1,
-                            "sync": false
-                        }
+                        "value": 60
                     },
                     "size": {
-                        "value": 1.5,
-                        "random": true,
-                        "anim": {
-                            "enable": false,
-                            "speed": 40,
-                            "size_min": 0.1,
-                            "sync": false
-                        }
-                    },
-                    "line_linked": {
-                        "enable": true,
-                        "distance": 10,
-                        "color": "#ffffff",
-                        "opacity": 0.3687847739990702,
-                        "width": 0.6413648243462091
-                    },
-                    "move": {
-                        "enable": true,
-                        "speed": 4,
-                        "direction": "bottom",
-                        "random": false,
-                        "straight": false,
-                        "out_mode": "out",
-                        "bounce": false,
-                        "attract": {
-                            "enable": false,
-                            "rotateX": 600,
-                            "rotateY": 1200
-                        }
+                        "value": 3
                     }
                 },
                 "interactivity": {
-                    "detect_on": "window",
                     "events": {
                         "onhover": {
                             "enable": true,
-                            "mode": "grab"
-                        },
-                        "onclick": {
-                            "enable": false,
-                            "mode": "bubble"
-                        },
-                        "resize": true
-                    },
-                    "modes": {
-                        "grab": {
-                            "distance": 400,
-                            "line_linked": {
-                                "opacity": 0.8
-                            }
-                        },
-                        "bubble": {
-                            "distance": 400,
-                            "size": 40,
-                            "duration": 2,
-                            "opacity": 8,
-                            "speed": 3
-                        },
-                        "repulse": {
-                            "distance": 100,
-                            "duration": 0.4
-                        },
-                        "push": {
-                            "particles_nb": 4
-                        },
-                        "remove": {
-                            "particles_nb": 2
+                            "mode": "repulse"
                         }
                     }
-                },
-                "retina_detect": true
+                }
+                
             }} >
 
             {props.children}    
